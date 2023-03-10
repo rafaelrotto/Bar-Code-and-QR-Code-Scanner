3f23017b401bd571cdeb8213cbfac718aa95ab51
Rafael <rafaelrotto@gmail.com>
2023-03-09 22:20:40 -0300

    creating a bar code scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BarcodeScanner
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,42 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import * as JsBarcode from 'jsbarcode';
+//import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnDestroy {
+export class HomePage implements OnInit, OnDestroy {
 
   qrCodeString = 'This is a secret qr code message';
+  barCodeString = 'This is secret bar code message';
   scannedResult: any;
+  barScannedResult: any;
+  content_visibility = '';
 
   constructor() { }
 
+  ngOnInit(): void {
+    JsBarcode("#barcode", "1234", {
+      //format: "pharmacode",
+      //lineColor: "#0aa",
+      //width: 4,
+      //height: 40,
+      displayValue: false
+    });
+  }
+
+  /*
+  startScan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+  */
   async checkPermission() {
     const status = await BarcodeScanner.checkPermission({ force: true });
     if (status.granted) {
@@ -29,8 +53,10 @@ export class HomePage implements OnDestroy {
       }
       await BarcodeScanner.hideBackground();
       document.querySelector('body')?.classList.add('scanner-active');
+      this.content_visibility = 'hidden';
       const result = await this.startScan();
       console.log(result);
+      this.content_visibility = '';
       if (result != null) {
         this.scannedResult = result;
         BarcodeScanner.showBackground();
@@ -41,8 +67,8 @@ export class HomePage implements OnDestroy {
       console.log(e);
     }
   }
-
   stopScan() {
+    this.content_visibility = '';
     BarcodeScanner.showBackground();
     BarcodeScanner.stopScan();
     document.querySelector('body')?.classList.remove('scanner-active');
